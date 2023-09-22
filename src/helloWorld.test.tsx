4b5e242f3aca379b6389fb4428d46bb55a341433
Zymus <moore.zyle@gmail.com>
2023-09-22 00:21:39 -0700

    Jest + React + TS

--- src/helloWorld.test.tsx
+++ src/helloWorld.test.tsx
@@ -1,11 +1,27 @@
 import { test, expect } from "@jest/globals";
 import helloWorld, { Label } from "./helloWorld";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/jest-globals';
+import userEvent from '@testing-library/user-event';
 
 test("helloWorld", (): void => {
   const value: string = helloWorld();
   expect(value).toBe("helloWorldaaa");
 });
 
-test("reactComponent", (): void => {
-  const element = (<Label />);
+test("jsxComponent", (): void => {
+  const labelValue = "labelValue";
+  render(<Label value={labelValue} />);
+  expect(screen.getByRole("button")).toHaveTextContent(labelValue);
+});
+
+test("useState onClick", async () => {
+  const labelValue = "labelValue";
+  const user = userEvent.setup();
+  render(<Label value={labelValue} />);
+  expect(screen.getByRole("button")).toHaveTextContent(labelValue);
+
+  await user.click(screen.getByRole("button"));
+  expect(screen.getByRole("button")).toHaveTextContent(labelValue + "a");
 });
--- src/helloWorld.tsx
+++ src/helloWorld.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+
 const helloWorld = (): string => "helloWorldaaa";
 
-export const Label = () => (<p>asdf</p>);
+export type LabelProps = {
+    value: string;
+};
+
+export const Label = (props: LabelProps): JSX.Element => {
+    const [value, setValue] = useState(props.value);
+
+    return (<button onClick={(e) => setValue(value + "a")}>{value}</button>);
+};
 
 export default helloWorld;
